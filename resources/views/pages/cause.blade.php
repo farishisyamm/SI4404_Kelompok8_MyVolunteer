4c88746ea77497766cd978f5a72f82935c8070da
Aishanabila <115605711+Aishanabila@users.noreply.github.com>
2023-01-10 01:28:46 +0700

    Add files via upload

---
+++ resources/views/pages/cause.blade.php
@@ -0,0 +1,249 @@
+@extends('main')
+@section('title', 'Cause')
+@section('content')
+<div class="hero overlay" style="background-image: url('images/hero_1.jpg')">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-6 text-center mx-auto">
+                <span class="subheading-white text-white mb-3" data-aos="fade-up">Causes</span>
+
+                <h1 class="heading text-white mb-2" data-aos="fade-up">The Causes</h1>
+                <p data-aos="fade-up" class=" mb-5 text-white lead text-white-50">There are still many people who need our help in this country</p>
+                <p data-aos="fade-up" data-aos-delay="100">
+                    <a href="#" class="btn btn-primary me-4">Follow Now</a>
+                </p>
+
+            </div>
+
+
+        </div>
+    </div>
+</div>
+
+
+
+<div class="section bg-light">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4 col-md-6">
+                <div class="causes-item bg-white">
+                    <a href="#"><img src="{{url('/images')}}/img_v_1-min.jpg" alt="Image" class="img-fluid mb-4 rounded"></a>
+                    <div class="px-4 pb-5 pt-3">
+
+                        <h3><a href="#">Food for the Hungry</a></h3>
+                        <p>According to data from the Global Hunger Index for 2021, the hunger rate in Indonesia is still at level 18, the third highest in Southeast Asia after Timor Leste and Kaos. Meanwhile, according to the Central Statistics Agency there are 17 million people or 6.1 percent of Indonesia's population still experiencing hunger.</p>
+
+                        <div class="progress mb-2">
+                            <div class="progress-bar" role="progressbar" style="width: 25%;" aria-valuenow="25"
+                                aria-valuemin="0" aria-valuemax="100">25%</div>
+                        </div>
+
+                        <div class="d-flex mb-4 justify-content-between amount">
+                            <div>250 Relawan</div>
+                            <div>1000 Relawan</div>
+                        </div>
+                        <div>
+                            <a href="#" class="btn btn-primary">Follow Now</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="causes-item bg-white">
+                    <a href="#"><img src="{{url('/images')}}/img_v_2-min.jpg" alt="Image" class="img-fluid mb-4 rounded"></a>
+                    <div class="px-4 pb-5 pt-3">
+                        <h3><a href="#">Education for Children</a></h3>
+                        <p>Data from the Ministry of Education, Culture, Research and Technology which was released in May 2022, stated that there were 75,303 children who dropped out of school in 2021 in Indonesia. The number of children who dropped out of school at the elementary school level was the highest, with 38,716 people.</p>
+
+                        <div class="progress mb-2">
+                            <div class="progress-bar" role="progressbar" style="width: 68%;" aria-valuenow="25"
+                                aria-valuemin="0" aria-valuemax="100">68%</div>
+                        </div>
+
+                        <div class="d-flex mb-4 justify-content-between amount">
+                            <div>680 Relawan</div>
+                            <div>1000 Relawan</div>
+                        </div>
+                        <div>
+                            <a href="#" class="btn btn-primary">Follow Now</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6">
+                <div class="causes-item bg-white">
+                    <a href="#"><img src="{{url('/images')}}/img_v_3-min.jpg" alt="Image" class="img-fluid mb-4 rounded"></a>
+                    <div class="px-4 pb-5 pt-3">
+                        <h3><a href="#">Support Livelihood</a></h3>
+                        <p>Indonesia's Poverty Rate Drops to 9.71% in September 2021. The improving domestic economy has driven the poverty rate down in Indonesia. The Central Statistics Agency (BPS) noted that the number of poor people in Indonesia decreased by 1.04 million to 26.5 million in March 2021 compared to March 2021.</p>
+
+                        <div class="progress mb-2">
+                            <div class="progress-bar" role="progressbar" style="width: 87%;" aria-valuenow="87"
+                                aria-valuemin="0" aria-valuemax="100">87%</div>
+                        </div>
+
+                        <div class="d-flex mb-4 justify-content-between amount">
+                            <div>$19,509.00</div>
+                            <div>1000 Relawan</div>
+                        </div>
+                        <div>
+                            <a href="#" class="btn btn-primary">Follow Now</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6">
+                <div class="causes-item bg-white">
+                    <a href="#"><img src="{{url('/images')}}/img_v_4-min.jpg" alt="Image" class="img-fluid mb-4 rounded"></a>
+                    <div class="px-4 pb-5 pt-3">
+
+                        <h3><a href="#">Food for the Hungry</a></h3>
+                        <p>According to data from the Global Hunger Index for 2021, the hunger rate in Indonesia is still at level 18, the third highest in Southeast Asia after Timor Leste and Kaos. Meanwhile, according to the Central Statistics Agency there are 17 million people or 6.1 percent of Indonesia's population still experiencing hunger.</p>
+
+                        <div class="progress mb-2">
+                            <div class="progress-bar" role="progressbar" style="width: 25%;" aria-valuenow="25"
+                                aria-valuemin="0" aria-valuemax="100">25%</div>
+                        </div>
+
+                        <div class="d-flex mb-4 justify-content-between amount">
+                            <div>250 Relawan</div>
+                            <div>1000 Relawan</div>
+                        </div>
+                        <div>
+                            <a href="#" class="btn btn-primary">Follow Now</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6">
+                <div class="causes-item bg-white">
+                    <a href="#"><img src="{{url('/images')}}/img_v_5-min.jpg" alt="Image" class="img-fluid mb-4 rounded"></a>
+                    <div class="px-4 pb-5 pt-3">
+                        <h3><a href="#">Education for Children</a></h3>
+                        <p>Data from the Ministry of Education, Culture, Research and Technology which was released in May 2022, stated that there were 75,303 children who dropped out of school in 2021 in Indonesia. The number of children who dropped out of school at the elementary school level was the highest, with 38,716 people.</p>
+
+                        <div class="progress mb-2">
+                            <div class="progress-bar" role="progressbar" style="width: 54%;" aria-valuenow="54"
+                                aria-valuemin="0" aria-valuemax="100">54%</div>
+                        </div>
+
+                        <div class="d-flex mb-4 justify-content-between amount">
+                            <div>540 Relawan</div>
+                            <div>1000 Relawan</div>
+                        </div>
+                        <div>
+                            <a href="#" class="btn btn-primary">Follow Now</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6">
+                <div class="causes-item bg-white">
+                    <a href="#"><img src="{{url('/images')}}/img_v_6-min.jpg" alt="Image" class="img-fluid mb-4 rounded"></a>
+                    <div class="px-4 pb-5 pt-3">
+                        <h3><a href="#">Support Livelihood</a></h3>
+                        <p>Indonesia's Poverty Rate Drops to 9.71% in September 2021. The improving domestic economy has driven the poverty rate down in Indonesia. The Central Statistics Agency (BPS) noted that the number of poor people in Indonesia decreased by 1.04 million to 26.5 million in March 2021 compared to March 2021.</p>
+
+                        <div class="progress mb-2">
+                            <div class="progress-bar" role="progressbar" style="width: 25%;" aria-valuenow="25"
+                                aria-valuemin="0" aria-valuemax="100">25%</div>
+                        </div>
+
+                        <div class="d-flex mb-4 justify-content-between amount">
+                            <div>250 Relawan</div>
+                            <div>1000 Relawan</div>
+                        </div>
+                        <div>
+                            <a href="#" class="btn btn-primary">Follow Now</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+
+
+<div class="section sec-instagram pb-0">
+    <div class="container mb-5">
+        <div class="row align-items-center">
+            <div class="col-lg-3" data-aos="fade-up">
+                <span class="subheading mb-3">Instagram</span>
+                <h2 class="heading">We Are In Instagram</h2>
+            </div>
+            <div class="col-lg-7" data-aos="fade-up" data-aos-delay="100">
+<<<<<<< HEAD
+                <p>
+                Want to know more about us? follow our instagram</p>
+=======
+                <p>IF YOU WANT TO KNOW MORE ABOUT US, CLICK ON INSTAGRAM !</p>
+>>>>>>> b42b0f425534bde0c0547ca4bbaafc589c03b992
+            </div>
+        </div>
+    </div>
+
+    <div class="instagram-slider-wrap" data-aos="fade-up" data-aos-delay="200">
+        <div class="instagram-slider" id="instagram-slider">
+
+            <div class="item">
+                <a class="instagram-item">
+                    <span class="icon-instagram"></span>
+                    <img src="{{url('/images')}}/img_v_8-min.jpg" alt="Image" class="img-fluid">
+                </a>
+            </div>
+
+            <div class="item">
+                <a class="instagram-item">
+                    <span class="icon-instagram"></span>
+                    <img src="{{url('/images')}}/img_v_2-min.jpg" alt="Image" class="img-fluid">
+                </a>
+            </div>
+
+            <div class="item">
+                <a class="instagram-item">
+                    <span class="icon-instagram"></span>
+                    <img src="{{url('/images')}}/img_v_3-min.jpg" alt="Image" class="img-fluid">
+                </a>
+            </div>
+
+            <div class="item">
+                <a class="instagram-item">
+                    <span class="icon-instagram"></span>
+                    <img src="{{url('/images')}}/img_v_4-min.jpg" alt="Image" class="img-fluid">
+                </a>
+            </div>
+
+            <div class="item">
+                <a class="instagram-item">
+                    <span class="icon-instagram"></span>
+                    <img src="{{url('/images')}}/img_v_5-min.jpg" alt="Image" class="img-fluid">
+                </a>
+            </div>
+
+            <div class="item">
+                <a class="instagram-item">
+                    <span class="icon-instagram"></span>
+                    <img src="{{url('/images')}}/img_v_6-min.jpg" alt="Image" class="img-fluid">
+                </a>
+            </div>
+
+            <div class="item">
+                <a class="instagram-item">
+                    <span class="icon-instagram"></span>
+                    <img src="{{url('/images')}}/img_v_7-min.jpg" alt="Image" class="img-fluid">
+                </a>
+            </div>
+
+
+        </div>
+    </div>
+
+</div>
+
+@endsection
